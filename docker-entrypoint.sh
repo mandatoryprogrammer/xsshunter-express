3108eded6df949acd78fcff83c4b09f847e9fd51
counter <counter@counters-MacBook-Air.local>
2023-01-14 12:26:04 -0800

    trying to get ssl

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ echo "Initializing SSL/TLS..."
 # Set up Greenlock
 # Test if --maintainer-email is required, we can set it via environment variables...
 npx greenlock init --config-dir /app/greenlock.d --maintainer-email $SSL_CONTACT_EMAIL
-npx greenlock add --subject $HOSTNAME --altnames "*.$XSS_HOSTNAME"
+npx greenlock add --subject $HOSTNAME --altnames "$HOSTNAME" "*.$XSS_HOSTNAME"
 
 echo "Starting server..."
 node server.js
