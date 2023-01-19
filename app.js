bdeb88b35fbd48e7503781475335f425e3509f1d
counter <counter@counters-MacBook-Air.local>
2023-01-18 20:11:20 -0800

    moving route

--- app.js
+++ app.js
@@ -210,16 +210,18 @@ async function get_app_server() {
     	const gzip = zlib.createGzip();
     	const output_gzip_stream = fs.createWriteStream(payload_fire_image_filename);
     	const input_read_stream = fs.createReadStream(multer_temp_image_path);
-
+        console.log("y");
     	// When the "finish" event is called we delete the original
     	// uncompressed image file left behind by multer.
         if (process.env.USE_CLOUD_STORAGE == "true"){
             const storage = new Storage();
             //creating a bucket instance
             const bucket = storage.bucket(process.env.BUCKET_NAME);
+            console.log("z");
             //compressing the file using gzip
             const gzip = zlib.createGzip();
             //uploading the gzipped file to GCS
+            console.log("hi");
             await bucket.upload(input_read_stream.pipe(gzip), {
                 gzip: true,
                 destination: payload_fire_image_filename,
