ab8844458a88ab51f45c8cd3a66ab257b64d2a6f
Tomitom13 <thomas.buton@isen.yncrea.Fr>
2023-11-07 11:21:07 +0100

    Add Rocket.chat integration

--- notification.js
+++ notification.js
@@ -1,6 +1,7 @@
 const nodemailer = require('nodemailer');
 const mustache = require('mustache');
 const fs = require('fs');
+const fetch = require("node-fetch");
 
 const XSS_PAYLOAD_FIRE_EMAIL_TEMPLATE = fs.readFileSync(
 	'./templates/xss_email_template.htm',
@@ -19,7 +20,7 @@ async function send_email_notification(xss_payload_fire_data) {
 	});
 
 	const notification_html_email_body = mustache.render(
-		XSS_PAYLOAD_FIRE_EMAIL_TEMPLATE, 
+		XSS_PAYLOAD_FIRE_EMAIL_TEMPLATE,
 		xss_payload_fire_data
 	);
 
@@ -34,4 +35,29 @@ async function send_email_notification(xss_payload_fire_data) {
 	console.log("Message sent: %s", info.messageId);
 }
 
+async function send_rocket_notification(xss_payload_fire_data) {
+
+	console.log('xss_payload_fire', xss_payload_fire_data.url)
+	const slack_message = {
+		text: `[XSS Hunter Express] XSS Payload Fired On ${xss_payload_fire_data.url}. See ${process.env.HOSTNAME}/admin`,
+	};
+
+	options = {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(slack_message)
+	}
+
+	fetch(process.env.ROCKET_WEBHOOK, options)
+		.then(response => response.json())
+		.then(data => {
+			console.log("Message sent to slack. Response data:", data); // Handle the response data here
+		})
+		.catch(error => {
+			console.error('Error:', error);
+		});
+}
 module.exports.send_email_notification = send_email_notification;
+module.exports.send_rocket_notification = send_rocket_notification;
